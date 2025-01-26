db90a5858ac33be6655f488c00f8e87acbfff8d5
Jarek <s24468@pjwstk.edu.pl>
2025-01-26 19:10:50 +0100

    construction changes

--- backend/model/construction.py
+++ backend/model/construction.py
@@ -1,16 +1,18 @@
 import json
 
-from backend.import_requirements import pd, os, train_test_split, DistilBertTokenizer, \
+from backend.import_requirements import pd, train_test_split, DistilBertTokenizer, \
     DistilBertForSequenceClassification, Trainer, TrainingArguments, torch, accuracy_score, Dataset
+from backend.import_requirements import os
 
 from transformers import TrainerCallback
 from sklearn.metrics import accuracy_score
 
 # Paths
 DATA_DIR = "../data/dataset.csv"
-CHART_EPOCHS = "charts/training_accuracy.png"
-COMPLETE_MODEL_DIR = "models/complete_model"
+CHART_EPOCHS = "../charts/training_accuracy.png"
+COMPLETE_MODEL_DIR = "complete_model"
 os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"  # Wyłączenie ostrzeżeń o symlinkach
+MODEL_LOG_DIR = "../log/model_log.json"
 
 
 # Data Preparation
@@ -106,7 +108,7 @@ def train_model(data):
         model.save_pretrained(COMPLETE_MODEL_DIR)
         tokenizer.save_pretrained(COMPLETE_MODEL_DIR)
 
-        with open("models/complete_model/log_history.json", "w") as f:
+        with open(MODEL_LOG_DIR, "w") as f:
             json.dump(trainer.state.log_history, f)
         return results
     except Exception as e:
@@ -133,3 +135,21 @@ class TrainingAccuracyCallback(TrainerCallback):
 
         # Log the training accuracy
         state.log_history.append({"accuracy": train_accuracy, "epoch": state.epoch})
+
+
+def construct():
+    if not os.path.exists(DATA_DIR):
+        raise Exception(f"Data directory does not exist: {DATA_DIR}")
+
+    try:
+        data = organize_data(DATA_DIR)
+        results = train_model(data)
+        print(f"Model trained and saved successfully."
+              f"Results: {results}")
+    except Exception as e:
+        print(f"Training failed: {e}")
+        raise
+
+
+if __name__ == '__main__':
+    construct()
